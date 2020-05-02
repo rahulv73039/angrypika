e8456d6b1cac378c3e15bf069e781a59859b0014
arahulv <56223991+rahulv73039@users.noreply.github.com>
2020-05-02 20:25:17 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,67 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//const config = require('./_config.json');
+
+// Variables
+var reminder;
+
+function sendReminder(channel, mention, message) {
+    channel.send(`${mention} ${message}`);
+}
+
+function parseReminderParams(text) {
+    var messageString = text.slice(3).join(" ");
+    console.log(messageString);
+
+    return {
+        mention: text[1],
+        time: text[2] * 1000 * 60,
+        message: messageString
+    };
+}
+
+
+// Register callbacks
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+    if (msg.content.startsWith('$')) {
+        const text = msg.content.split(" ");
+        console.log(text);
+        var command = text[0];
+        console.log(command);
+
+        if (command === '$ping') {
+            msg.reply('pong');
+        }
+
+        // $remind <Mention> <Time> <Message>
+        if (command === '$remind') {
+            var param = parseReminderParams(text) // in milliseconds
+            if(isNaN(param.time)||param.time<600){
+                msg.reply("Enter Time duration or I will go on a frenzy :) \n The syntax is as follows $remind <Mention> <Time> <Message>");
+            }
+            else{
+                console.log(param.time);
+                reminder = setInterval(sendReminder, param.time, msg.channel, param.mention, param.message);
+            }
+
+        }
+
+        if (command === '$clear') {
+            clearInterval(reminder);
+        }
+        if(command == '$remindonce'){
+            var param = parseReminderParams(text);
+            
+            reminder = setTimeout(sendReminder, param.time, msg.channel, param.mention, param.message);
+        }
+    }
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
 
